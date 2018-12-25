21e3ed6e8876d1e4b599166e0948ec196a7cb0ce
Matt Simonis <matts12290@gmail.com>
2018-12-24 18:31:17 -0800

    updated to use environment variable for prismic repository

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,5 +1,10 @@
+require('dotenv').config();
+
+// Get the repository from the environment variables.
+const repository = process.env.PRISMIC_REPOSITORY || 'recipe-box';
+
 module.exports = {
-  apiEndpoint: 'https://recipe-box.prismic.io/api/v2',
+  apiEndpoint: `https://${repository}.prismic.io/api/v2`,
 
   linkResolver(doc) {
     if (doc.type === 'recipe') {
