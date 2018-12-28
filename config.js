1cf22ea0b1c56202ce2a9e8994a2c67ecff46645
Matt Simonis <matts12290@gmail.com>
2018-12-28 14:28:25 -0800

    added maxage to public

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ module.exports = (() => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(compression());
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'public'), { maxage: '1d' }));
 
   app.locals.basedir = path.join(__dirname, 'public');
 
