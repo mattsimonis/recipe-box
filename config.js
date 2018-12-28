b2133effb27dcc851933556f9828d86115880006
Matt Simonis <matts12290@gmail.com>
2018-12-28 14:18:52 -0800

    refactoring

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const favicon = require('serve-favicon');
-const bodyParser = require('body-parser');
+const sassMiddleware = require('node-sass-middleware');
 const compression = require('compression');
 const path = require('path');
 
@@ -11,11 +11,17 @@ module.exports = (() => {
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'pug');
   app.use(favicon('public/images/favicon.png'));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(sassMiddleware({
+    src: path.join(__dirname),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: true,
+    outputStyle: 'compressed',
+    maxAge: 86400,
+  }));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
   app.use(compression());
-  app.use(express.static(path.join(__dirname, 'public'), { maxage: '1d' }));
-  app.use('/bulma', express.static(path.join(__dirname, 'node_modules/bulma/css/'), { maxage: '1d' }));
+  app.use(express.static(path.join(__dirname, 'public')));
 
   app.locals.basedir = path.join(__dirname, 'public');
 
