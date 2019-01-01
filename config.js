9b72b20d6dac7b63378f7e7f259336d4d3c093df
Matt Simonis <matts12290@gmail.com>
2019-01-01 12:16:32 -0800

    updated font and ingredient groups

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ module.exports = (() => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(compression());
-  app.use(express.static(path.join(__dirname, 'public'), { maxage: maxage }));
+  app.use(express.static(path.join(__dirname, 'public'), { maxage }));
 
   app.locals.basedir = path.join(__dirname, 'public');
 
